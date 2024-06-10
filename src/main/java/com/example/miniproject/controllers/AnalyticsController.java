f06337592d242e9ea0b4d24ceb44f8336b91fa33
tamruthavarshini <t.varsha0529@example.com>
2024-06-10 19:46:42 +0530

    analytics is updated

--- src/main/java/com/example/miniproject/controllers/AnalyticsController.java
+++ src/main/java/com/example/miniproject/controllers/AnalyticsController.java
@@ -2,6 +2,7 @@ package com.example.miniproject.controllers;
 import com.example.miniproject.model.student.Outpass;
 import com.example.miniproject.repository.OutpassRepository;
 import com.example.miniproject.services.AnalyticsService;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
@@ -9,8 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 import static org.apache.commons.lang3.time.DateUtils.parseDate;
 
@@ -24,13 +24,34 @@ public class AnalyticsController {
     private AnalyticsService analyticsService;
     @CrossOrigin(origins = "*")
     @GetMapping("/count")
-    public int convertDateToLocalDateTime(@RequestHeader("Authorization") String token,@RequestParam("date") @DateTimeFormat(pattern = "yyyy-MM-dd") String dateString) {
+    public List<Integer> convertDateToLocalDateTime(@RequestHeader("Authorization") String token,@RequestParam("date") @DateTimeFormat(pattern = "yyyy-MM-dd") String dateString) {
         Date date = analyticsService.parseDate(dateString);
         LocalDateTime today =analyticsService.convertToLocalDateTime(date);
+        LocalDateTime day6 =today.minusDays(1);
+        LocalDateTime day5 =today.minusDays(2);
+        LocalDateTime day4 =today.minusDays(3);
+        LocalDateTime day3 =today.minusDays(4);
+        LocalDateTime day2 =today.minusDays(5);
+        LocalDateTime day1 =today.minusDays(6);
+
         LocalDateTime tomorrow=today.plusDays(1);
-        List<Outpass> x1= outpassRepository.findByCreatedAtGreaterThanEqual(today);
-        List<Outpass> x2= outpassRepository.findByCreatedAtGreaterThanEqual(tomorrow);
-        return x1.size()-x2.size()  ;
+        List<Outpass> x2= outpassRepository.findByCreatedAtGreaterThanEqual(today);
+        List<Outpass> x1= outpassRepository.findByCreatedAtGreaterThanEqual(tomorrow);
+        List<Outpass> x3= outpassRepository.findByCreatedAtGreaterThanEqual(day6);
+        List<Outpass> x4= outpassRepository.findByCreatedAtGreaterThanEqual(day5);
+        List<Outpass> x5= outpassRepository.findByCreatedAtGreaterThanEqual(day4);
+        List<Outpass> x6= outpassRepository.findByCreatedAtGreaterThanEqual(day3);
+        List<Outpass> x7= outpassRepository.findByCreatedAtGreaterThanEqual(day2);
+        List<Outpass> x8= outpassRepository.findByCreatedAtGreaterThanEqual(day1);
+        List<Integer> list=new ArrayList<>();
+        list.add(x8.size()-x7.size());
+        list.add(x7.size()-x6.size());
+        list.add(x6.size()-x5.size());
+        list.add(x5.size()-x4.size());
+        list.add(x4.size()-x3.size());
+        list.add(x3.size()-x2.size());
+        list.add(x2.size()-x1.size());
+        return list;
     }
 
 
